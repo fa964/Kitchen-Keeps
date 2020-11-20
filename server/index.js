77ad528ec41f31b099117a7fe4a9d23854602e20
Farhan Ahmed <fa964@nyu.edu>
2020-11-19 20:40:40 -0500

    second commit

--- server/index.js
+++ server/index.js
@@ -14,7 +14,6 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://fa964:yeetzlit1492@cluster0.iogm1.mongodb.net/<dbname>?retryWrites=true&w=majority'
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
